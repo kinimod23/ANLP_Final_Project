e9127ac9c9053a4862d3d2d93722fcf8938417ea
fhewett <32869312+fhewett@users.noreply.github.com>
2018-01-29 10:07:25 +0100

    script for removing punctuation,stopwords etc

--- stopwords.py
+++ stopwords.py
@@ -18,15 +18,26 @@ for elem1 in to_delete:
 for elem in string.punctuation:
     stop_words.append(elem)
 
+
 tokenized_files = ["just_hl_guardian_tokenized.txt", "just_hl_article_tokenized.txt", 
                    "just_hl_article_guardian_tokenized.txt", "just_hl_sun_tokenized.txt"]
 
 for t_file in tokenized_files:
     with open("filtered" + t_file, "w") as output_file:
     
-        to_be_filtered = open(t_file).read()
-        words = to_be_filtered.split()
-        for word in words:
-            word = re.sub(r"^(\')", "", word) #getting rid of the ' at the beginning of some words
-            if word.lower() not in stop_words:
-                print(word, file=output_file)
+        to_be_filtered = open(t_file).readlines()
+        corpus_list = [line.strip() for line in to_be_filtered] 
+        corpus_nlist = [each_word.split() for each_word in corpus_list]
+        for sentence in range(0, len(corpus_nlist)):
+            for word in corpus_nlist[sentence]:
+                word = re.sub(r"^(\')", "", word) #getting rid of the ' at the beginning of some words
+                if word.lower() not in stop_words:
+                    output_file.write(word)
+                    output_file.write(" ")
+            output_file.write("\n") #line break after each article/hl
+
+                
+
+                
+                
+                
