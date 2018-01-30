22f31336cac221e82c0b480f9a21139e7b3f76d7
Tim P <tpatzelt@uos.de>
2018-01-30 15:50:51 +0100

    Add files via upload
    noch neuer und besser

--- k_means.py
+++ k_means.py
@@ -13,25 +13,37 @@ class kmeans():
 
 
     def cluster(self,k):
-        labels = KMeans(n_clusters=k,random_state=self.r_state).fit_predict(self.feature_vector)
+        clustering = KMeans(n_clusters=k,random_state=self.r_state)
+        labels = clustering.fit_predict(self.feature_vector)
         self.labels = labels
-        return labels
+        return(labels,clustering.inertia_)
 
     def visualize_data(self,fname):
 
         X_reduced = PCA(n_components=2).fit_transform(self.feature_vector)
         fig = plt.figure()
-        centers = np.random.randn(4, 2)
+
         ax = fig.add_subplot(111)
         scatter = ax.scatter(X_reduced[:,0], X_reduced[:,1], c=self.labels, s=5)
-        #for i, j in centers:
-         #   ax.scatter(i, j, s=50, c='red', marker='+')
+
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         plt.colorbar(scatter)
 
         plt.savefig(fname,dpi=600)
 
+    def plot_elbow(self,k_range,fname):
+        distorsions = []
+        for k in k_range:
+            labels, score = k_means.cluster(k)
+            distorsions.append(score)
+
+        plt.figure(figsize=(15, 5))
+        plt.xlabel("number of clusters: k")
+        plt.ylabel("Sum of squared distances of samples to their closest cluster center.")
+        plt.plot(k_range, distorsions)
+        plt.grid(True)
+        plt.savefig(fname,dpi=600)
 
 if __name__ == '__main__':
     data = [[0,1,2,3,4,5],[0,1,4,5,6,3],[7,4,3,2,3,4]]
@@ -40,3 +52,6 @@ if __name__ == '__main__':
     labels = k_means.cluster(2)
     print(list(zip(data,labels)))
     k_means.visualize_data("cluster1")
+
+    k_means.plot_elbow(range(1,3),"Elbow plot")
+
