c6a06f6e4d2b105dd9ef46a049b24737e9bf1698
unknown <simon.untergasser@campus.tu-berlin.de>
2018-01-30 15:08:47 +0100

    Made LDA a class. We now get a N-dim FeatureVector per article

--- lda_Simon.py
+++ lda_Simon.py
@@ -3,48 +3,69 @@ import gensim
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
-#import scikit
+from matplotlib import pyplot as plt
 
 
 ######### Das gehört zu Simons Änderungen ##########
 from pprint import pprint
 from textblob import TextBlob
 
-def createCorpus(filename1, filename2, stopwords):
-    with open(filename1,encoding="utf-8") as f1:
-        with open(filename2, encoding="utf-8") as f2:
-            all_articles = f1.readlines()
-            all_articles.extend(f2.readlines())
-            tokens = [[token for token in article.strip().split(" ") if token not in stopword] for article in all_articles[:20]]
-    return tokens
+class LDA():
+    def __init__(self, filename1, filename2, stopwords, num_topics=10, no_below=20, no_above=0.5):
+        self.num_topics = num_topics
+        self.corpus = self.createCorpus(filename1, filename2, stopwords)
+        self.model_corpus, self.dictionary = self.createModelCorpus(no_below, no_above)
+        self.model = self.train_lda()
+        self.corpus_feature_vectors = self.apply_lda()
+        self.final_output = self.createFinalOutput()
 
-def createModelCorpus(tokens):
-    dictionary = corpora.Dictionary(tokens)
-    dictionary.filter_extremes(no_below=20, no_above=0.5)
-    corpus = [dictionary.doc2bow(token) for token in tokens]
-    return corpus
+    def createCorpus(self, filename1, filename2, stopwords):
+        with open(filename1,encoding="utf-8") as f1:
+            with open(filename2, encoding="utf-8") as f2:
+                all_articles = f1.readlines()
+                all_articles.extend(f2.readlines())
+                corpus = [[token for token in article.strip().split(" ") if token not in stopword] for article in all_articles]
+        return corpus
 
-def train_lda(tokens):
-    '''applies LDA to tokenized articles in a txt-file where each article is
-    separated by a newsline'''
-    dictionary = corpora.Dictionary(tokens)
-    dictionary.filter_extremes(no_below=20, no_above=0.5)
-    corpus = [dictionary.doc2bow(token) for token in tokens]
-    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word=dictionary, passes=10)
-    return ldamodel
+    def createModelCorpus(self, no_below, no_above):
+        dictionary = corpora.Dictionary(self.corpus)
+        dictionary.filter_extremes(no_below=no_below, no_above=no_above)
+        model_corpus = [dictionary.doc2bow(token) for token in self.corpus]
+        return model_corpus, dictionary
 
-def apply_lda(lda_model, tokens):
-    corpus_feature_vectors = []
-    for article in tokens:
-        corpus_feature_vectors.append(lda_model(token))
-    return corpus_feature_vectors
+    def train_lda(self):
+        '''applies LDA to tokenized articles in a txt-file where each article is
+        separated by a newsline'''
+        ldamodel = gensim.models.ldamodel.LdaModel(self.model_corpus, num_topics=self.num_topics, id2word=self.dictionary, passes=10)
+        return ldamodel
+
+    def apply_lda(self):
+        corpus_feature_vectors = []
+        for article in self.model_corpus:
+            corpus_feature_vectors.append(self.model[article])
+        return corpus_feature_vectors
+
+    def createFinalOutput(self):
+        output = []
+        for vec in self.corpus_feature_vectors:
+            vec_dic = dict(vec)
+            output.append([(vec_dic[topic] if topic in vec_dic else 0) for topic in range(10)])
+        return output
 
 
 stopword = stopwords.words("english") + list(string.punctuation)
 
-corpus = createCorpus("Corpora/filtered/filteredjust_hl_article_guardian_tokenized.txt","Corpora/filtered/filteredjust_hl_article_tokenized.txt" ,stopword)
-model_corpus = createModelCorpus(corpus)
-lda_model = train_lda(model_corpus)
-corpus_feature_vectors = apply_lda(lda_model, tokens)
+filename1 = "Corpora/filtered/filteredjust_hl_article_guardian_tokenized.txt"
+filename2 = "Corpora/filtered/filteredjust_hl_article_tokenized.txt"
 
-pprint(model.print_topics(num_topics=10, num_words=15))
+
+# no_below : No words which appear in less than X articles
+# no_above : No words which appear in more than X % of the articles
+lda = LDA(filename1, filename2 ,stopword, num_topics=10, no_below=20, no_above=0.8)
+
+corpus_feature_vectors = lda.corpus_feature_vectors
+output = lda.final_output
+
+pprint(output[:2])
+
+plt.plot(output)
